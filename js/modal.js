20ea6b8b75e76c184dc0a987408010c96e93e0d1
Evgeniy Boyko <jekaboyko0605@gmail.com>
2024-01-16 19:26:27 +0200

    Добавил возможность закрывать модалку при клике на backdrop и ESC

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,33 @@
-(() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-    };
+const refs = {
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector("[data-modal-close]"),
+  backdrop: document.querySelector("[data-modal]"),
+};
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener("click", onModalOpen);
+refs.closeModalBtn.addEventListener("click", onModalClose);
+refs.backdrop.addEventListener("click", onBackDropClick);
 
-    function toggleModal() {
-        document.body.classList.toggle("modal-open");
-        refs.modal.classList.toggle("backdrop--hidden");
-    }
-})();
+function onModalOpen() {
+  window.addEventListener("keydown", onEscapeKeypress);
+  document.body.classList.add("modal-open");
+  refs.backdrop.classList.remove("backdrop--hidden");
+}
+
+function onModalClose() {
+  window.removeEventListener("keydown", onEscapeKeypress);
+  document.body.classList.remove("modal-open");
+  refs.backdrop.classList.add("backdrop--hidden");
+}
+
+function onBackDropClick(event) {
+  if (event.target === event.currentTarget) {
+    onModalClose();
+  }
+}
+
+function onEscapeKeypress(event) {
+  if (event.code === "Escape") {
+    onModalClose();
+  }
+}
