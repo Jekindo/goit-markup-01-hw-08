a4c3834cb216bf63a4d693d9ab8cac9599031c6b
Evgeniy Boyko <jekaboyko0605@gmail.com>
2024-01-16 19:27:04 +0200

    Создал portfolio.js и заполнил данными о портфолио

---
+++ js/data/portfolio.js
@@ -0,0 +1,126 @@
+export default [
+  {
+    images: {
+      webp: "../../images/techcrack.webp",
+      webp_2x: "../../images/techcrack@2x.webp",
+      jpg: "../../images/techcrack.jpg",
+      jpg_2x: "../../images/techcrack@2x.jpg",
+    },
+    alt: "Ноутбук Mac Book Pro с открытым сайтом Технокряк",
+    name: "Технокряк",
+    label: "Веб-сайт",
+    description:
+      "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании или частном лице.",
+    tag: "web-sites",
+  },
+  {
+    images: {
+      webp: "../../images/basketball.jpg",
+      webp_2x: "../../images/basketball@2x.webp",
+      jpg: "../../images/basketball.jpg",
+      jpg_2x: "../../images/basketball@2x.jpg",
+    },
+    alt: "Два баскетболиста соревнуются за мяч",
+    name: "Постер New Orlean vs Golden Star",
+    label: "Дизайн",
+    description: "Lorem ipsum dolor sit amet.",
+    tag: "design",
+  },
+  {
+    images: {
+      webp: "../../images/seafood.webp",
+      webp_2x: "../../images/seafood@2x.webp",
+      jpg: "../../images/seafood.jpg",
+      jpg_2x: "../../images/seafood@2x.jpg",
+    },
+    alt: "Логотип ресторана Seafood",
+    name: "Ресторан Seafood",
+    label: "Приложение",
+    description:
+      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti repellendus quisquam qui dolorum veniam. Quae harum reiciendis culpa iusto impedit exercitationem architecto recusandae itaque maiores ad, tempore aliquam. Asperiores, excepturi.",
+    tag: "apps",
+  },
+  {
+    images: {
+      webp: "../../images/headphones.webp",
+      webp_2x: "../../images/headphones@2x.webp",
+      jpg: "../../images/headphones.jpg",
+      jpg_2x: "../../images/headphones@2x.jpg",
+    },
+    alt: "Полноразмерные наушники",
+    name: "Проект Prime",
+    label: "Маркетинг",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ea illum molestiae autem natus consequatur doloribus explicabo, tempore dolor quo alias, sunt veniam aliquid mollitia ipsam excepturi aliquam labore magni vero. Itaque dignissimos quasi temporibus ad. Nobis saepe, voluptatum neque iusto, necessitatibus asperiores aliquam eum maxime ipsum eaque eos, quae expedita commodi dolorum? Exercitationem non quibusdam officiis quas reiciendis quaerat, necessitatibus perferendis cum modi aspernatur dicta nobis explicabo ipsa placeat ea consectetur eveniet quisquam provident fuga amet ut neque obcaecati itaque nesciunt. Tempora minima atque quae eum optio nostrum? Placeat mollitia perspiciatis ipsum minima quasi nemo vero ducimus harum facilis.",
+    tag: "marketing",
+  },
+  {
+    images: {
+      webp: "../../images/boxes.webp",
+      webp_2x: "../../images/boxes@2x.webp",
+      jpg: "../../images/boxes.jpg",
+      jpg_2x: "../../images/boxes@2x.jpg",
+    },
+    alt: "Две коробки",
+    name: "Проект Boxes",
+    label: "Приложение",
+    description:
+      " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad aut non, suscipit quas animi eius.",
+    tag: "apps",
+  },
+  {
+    images: {
+      webp: "../../images/inspiration.webp",
+      webp_2x: "../../images/inspiration@2x.webp",
+      jpg: "../../images/inspiration.jpg",
+      jpg_2x: "../../images/inspiration@2x.jpg",
+    },
+    alt: "Apple IMac",
+    name: "Inspiration has no Borders",
+    label: "Веб-сайт",
+    description: "Lorem, ipsum.",
+    tag: "web-sites",
+  },
+  {
+    images: {
+      webp: "../../images/limitededition.webp",
+      webp_2x: "../../images/limitededition@2x.webp",
+      jpg: "../../images/limitededition.jpg",
+      jpg_2x: "../../images/limitededition@2x.jpg",
+    },
+    alt: "Журнал с изданием Limited Edition",
+    name: "Издание Limited Edition",
+    label: "Дизайн",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ea illum molestiae autem natus consequatur doloribus explicabo, tempore dolor quo alias, sunt veniam aliquid mollitia ipsam excepturi aliquam labore magni vero. Itaque dignissimos quasi temporibus ad. Nobis saepe, voluptatum neque iusto, necessitatibus asperiores aliquam eum maxime ipsum eaque eos, quae expedita commodi dolorum? Exercitationem non quibusdam officiis quas reiciendis quaerat, necessitatibus perferendis cum modi aspernatur dicta nobis explicabo ipsa placeat ea consectetur eveniet quisquam provident fuga amet ut neque obcaecati itaque nesciunt. Tempora minima atque quae eum optio nostrum? Placeat mollitia perspiciatis ipsum minima quasi nemo vero ducimus harum facilis.",
+    tag: "design",
+  },
+  {
+    images: {
+      webp: "../../images/lab.webp",
+      webp_2x: "../../images/lab@2x.webp",
+      jpg: "../../images/lab.jpg",
+      jpg_2x: "../../images/lab@2x.jpg",
+    },
+    alt: "Ярлык lAB EL TAG",
+    name: "Проект LAB",
+    label: "Маркетинг",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ea illum molestiae autem natus consequatur doloribus explicabo, tempore dolor quo alias, sunt veniam aliquid mollitia ipsam excepturi aliquam labore magni vero. Itaque dignissimos quasi temporibus ad. Nobis saepe, voluptatum neque iusto, necessitatibus asperiores aliquam eum maxime ipsum eaque eos, quae expedita commodi dolorum? Exercitationem non quibusdam officiis quas reiciendis quaerat, necessitatibus perferendis cum modi aspernatur dicta nobis explicabo ipsa placeat ea consectetur eveniet quisquam provident fuga amet ut neque obcaecati itaque nesciunt. Tempora minima atque quae eum optio nostrum? Placeat mollitia perspiciatis ipsum minima quasi nemo vero ducimus harum facilis.",
+    tag: "marketing",
+  },
+  {
+    images: {
+      webp: "../../images/growingbusiness.webp",
+      webp_2x: "../../images/growingbusiness@2x.webp",
+      jpg: "../../images/growingbusiness.jpg",
+      jpg_2x: "../../images/growingbusiness@2x.jpg",
+    },
+    alt: "Apple IMac",
+    name: "Growing Business",
+    label: "Приложение",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ea illum molestiae autem natus consequatur doloribus explicabo, tempore dolor quo alias, sunt veniam aliquid mollitia ipsam excepturi aliquam labore magni vero. Itaque dignissimos quasi temporibus ad. Nobis saepe.",
+    tag: "apps",
+  },
+];
